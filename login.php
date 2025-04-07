c3ec517489c06063d5a51b5a5e00e2fb8c9c05c7
saintmuka <c4ff0e@gmail.com>
2025-04-07 13:48:28 +0300

    new

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = trim($_POST["email"]);
+  $password = trim($_POST["password"]);
+
+
+  $safeEmail = htmlspecialchars($email);
+  $safePassword = htmlspecialchars($password);
+
+
+  $hashedPassword = password_hash($safePassword, PASSWORD_DEFAULT);
+
+
+  $file = fopen("users.txt", "a");
+  fwrite($file, "Email: $safeEmail | Password: $hashedPassword\n");
+  fclose($file);
+
+  echo "<body style='background-color:black; color:#d3d3d3; text-align:center; font-family:Arial'>";
+  echo "<h1>Регистрация прошла успешно!</h1>";
+  echo "<p>Данные сохранены на сервере.</p>";
+  echo "<a href='index.html' style='color:#d3d3d3;'>Вернуться на главную</a>";
+  echo "</body>";
+} else {
+  header("Location: index.html");
+  exit();
+}
+
